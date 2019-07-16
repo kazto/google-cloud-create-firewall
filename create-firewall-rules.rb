934494df045ae0dd45c6d3d5573315ebb81e7427
ik-fib <ik11235@gmail.com>
2019-07-16 12:15:31 +0900

    無停止での自動更新対応に2種類のフラグでルール作成→古い方を削除するように

--- create-firewall-rules.rb
+++ create-firewall-rules.rb
@@ -1,11 +1,24 @@
 #! /usr/bin/env ruby
 
+require 'json'
+
+a_or_b = nil
+
+list_json = `gcloud compute firewall-rules list --format=json`
+hash = JSON.load(list_json)
+hash.each do |data|
+    a_or_b = "b" if data["name"].start_with?("block-cost-country-a-")
+    a_or_b = "a" if data["name"].start_with?("block-cost-country-b-")
+end
+
+a_or_b = "a" if a_or_b == nil
+
 ['cn', 'au'].each do |country|
     ips = File.open(country + '.txt').readlines.map{|v| v.chomp}
     cnt = 1
 
     ips.each_slice(254) do |ary|
-        name = 'block-cost-country-' + country + ('-%03d' % cnt)
+        name = 'block-cost-country-' + a_or_b + '-' + country + ('-%03d' % cnt)
 
         cmd_create = [
             'gcloud compute firewall-rules create',
@@ -22,4 +35,24 @@
         cnt += 1
     end
 
+    list_json = `gcloud compute firewall-rules list --format=json`
+    hash = JSON.load(list_json)
+    delete_names = []
+    hash.each do |data|
+        if (a_or_b == "a" && data["name"].start_with?("block-cost-country-b-")) ||
+            (a_or_b == "b" && data["name"].start_with?("block-cost-country-a-"))
+            delete_names.push(data["name"])
+        end
+    end
+
+    if delete_names != []
+        cmd_create = [
+            'gcloud compute firewall-rules delete',
+            delete_names.join(" "),
+            '--quiet',
+        ]
+        cmdstr_create = cmd_create.join(" ")
+        puts cmdstr_create
+        system cmdstr_create
+    end
 end
